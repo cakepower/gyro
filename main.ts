2d648f19987fb398b5ec8a63956d7243c198be5c
cakepower <32028127+cakepower@users.noreply.github.com>
2021-01-11 19:45:02 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,7 +5,7 @@ strip.showColor(neopixel.colors(NeoPixelColors.Black))
 basic.pause(100)
 basic.forever(function () {
     basic.showNumber(input.acceleration(Dimension.X))
-    gyro_sensor = input.acceleration(Dimension.X) / 5
+    gyro_sensor = input.acceleration(Dimension.X)
     if (gyro_sensor > 0) {
         strip.showColor(neopixel.colors(NeoPixelColors.Indigo))
         basic.pause(500)
