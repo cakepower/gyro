8dc5abc3e6a745aee930f0550d10d5f08e0a9dc6
cakepower <32028127+cakepower@users.noreply.github.com>
2021-01-13 20:00:36 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,27 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 1) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Red))
+    } else if (receivedNumber == 2) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Blue))
+    } else {
+        strip.showColor(neopixel.colors(NeoPixelColors.Black))
+    }
+})
 let gyro_sensor = 0
-let strip = neopixel.create(DigitalPin.P11, 24, NeoPixelMode.RGB)
+let strip: neopixel.Strip = null
+radio.setGroup(25)
+strip = neopixel.create(DigitalPin.P11, 24, NeoPixelMode.RGB)
 strip.setBrightness(20)
 strip.showColor(neopixel.colors(NeoPixelColors.Black))
 basic.pause(100)
 basic.forever(function () {
     basic.showNumber(input.acceleration(Dimension.Z))
     gyro_sensor = input.acceleration(Dimension.Z)
-    if (gyro_sensor < 5) {
+    if (gyro_sensor < -50) {
         strip.showColor(neopixel.colors(NeoPixelColors.Indigo))
-        basic.pause(100)
     } else if (gyro_sensor > 90) {
         strip.showColor(neopixel.colors(NeoPixelColors.Orange))
-        basic.pause(100)
     } else {
         strip.showColor(neopixel.colors(NeoPixelColors.Black))
-        basic.pause(100)
     }
 })
